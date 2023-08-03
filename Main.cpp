4300ab35b18b404ab507ac6ff480dc17b42801e0
hwangs12 <sungjun94@gmail.com>
2023-08-02 20:49:42 -0400

    want to assign value to custom type

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Complex.h"
+
+int main()
+{
+    // How to make below possible? Currently, conversion error from Real to double is not possible.
+    // use of copy constructor? Ref https://www.geeksforgeeks.org/copy-constructor-vs-assignment-operator-in-c/#
+    // Real a = 13.45
+}
